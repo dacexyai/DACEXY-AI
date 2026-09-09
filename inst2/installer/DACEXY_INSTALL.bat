79d336c442e27c8ae8478d3fe9aec473be5f3814
dacexyai <admin.dacexy@gmail.com>
2026-09-09 18:33:23 +0530

    Delete inst2/installer directory

--- inst2/installer/DACEXY_INSTALL.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-title Dacexy Installer
-color 0D
-powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0install.ps1"
-pause
--- inst2/installer/UNINSTALL.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-title DACEXY AI Uninstaller
-powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0uninstall.ps1"
-echo DACEXY Agent runtime removed.
-pause
